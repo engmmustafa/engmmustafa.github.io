604c916cdba9ec5a871fd401937d1531ca5636be
Muhammed Mustafa <muhammed.mustafa9831@gmail.com>
2024-12-08 02:45:14 +0300

    Add personal webpage

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-// Adding interactivity to the profile image hover
+// Adding interactivity to the profile images hover
 window.onload = function() {
-    const profileImage = document.getElementById('profileImage');
-    profileImage.addEventListener('mouseover', function() {
-        profileImage.style.transform = 'scale(1.1)';
+    const profileImageLeft = document.getElementById('profileImageLeft');
+    const profileImageRight = document.getElementById('profileImageRight');
+
+    profileImageLeft.addEventListener('mouseover', function() {
+        profileImageLeft.style.transform = 'scale(1.1)';
     });
-    profileImage.addEventListener('mouseout', function() {
-        profileImage.style.transform = 'scale(1)';
+    profileImageLeft.addEventListener('mouseout', function() {
+        profileImageLeft.style.transform = 'scale(1)';
+    });
+
+    profileImageRight.addEventListener('mouseover', function() {
+        profileImageRight.style.transform = 'scale(1.1)';
+    });
+    profileImageRight.addEventListener('mouseout', function() {
+        profileImageRight.style.transform = 'scale(1)';
     });
 }
