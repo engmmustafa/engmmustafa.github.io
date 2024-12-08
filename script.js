bea2524a0c8a2224e29418f123e09ef20dfe8360
Muhammed Mustafa <muhammed.mustafa9831@gmail.com>
2024-12-08 03:20:14 +0300

    Add personal webpage

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-// Adding interactivity to photo hover
-window.onload = function () {
-    const overlayPhoto = document.querySelector('.overlay-photo');
-    overlayPhoto.addEventListener('mouseover', () => {
-        overlayPhoto.style.transform = 'scale(1.2) translate(-50%, -50%)';
+function setLanguage(lang) {
+    const elements = document.querySelectorAll('[data-lang]');
+    elements.forEach(el => {
+        el.style.display = el.getAttribute('data-lang') === lang ? '' : 'none';
     });
-    overlayPhoto.addEventListener('mouseout', () => {
-        overlayPhoto.style.transform = 'scale(1) translate(-50%, -50%)';
-    });
-};
+}
